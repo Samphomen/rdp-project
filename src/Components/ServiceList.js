00ec7a05710460605a2b120b8b0c5b498e28460f
Samphomen <ishaqcole@gmail.com>
2024-01-27 17:36:59 +0100

    first commit

---
+++ src/Components/ServiceList.js
@@ -0,0 +1,33 @@
+import homeSpace from '../Images/homeSpace.jpg'
+import solutions from '../Images/solutions.jpeg'
+import socialWorker from '../Images/socialWorker.jpeg'
+import houseCleaner from '../Images/houseCleaner.jpeg'
+
+
+
+export const services = [
+    {
+        id: 1,
+        image: homeSpace,
+        heading: "Independent Living Support Services",
+        content: "A supported living business provides assistance and support services for individuals with physical, intellectual, or developmental disabilities to live independently in their own homes or apartments. This can include help with daily tasks, transportation, and access to community resources, allowing individuals to maintain their independence while receiving necessary support"
+    },
+    {
+        id: 2,
+        image: houseCleaner,
+        heading: "Compassionate Care Solutions",
+        content: "A supported living business provides assistance and support services for individuals with disabilities or special needs to live independently and receive support as needed. This may include assistance with daily activities, access to community resources and support for managing personal finances and healthcare. Through this service, individuals can maintain their independence while still receiving the necessary support to lead fulfilling lives"
+    },
+    {
+        id: 3,
+        image: socialWorker,
+        heading: "Personalised Care Plan",
+        content: "Our team creates personalised care plans tailored to the individual's needs, ensuring the highest quality of support"
+    },
+    {
+        id: 4,
+        image: solutions,
+        heading: "Assisted Independence Solutions",
+        content: "A supported living service provides assistance and support services to individuals who require help to live independently in their home. This can include assistance with daily tasks, access to community resources, and help with managing health and personal care needs. The goal of this service is to promote independence and enhance the quality of life for individuals with physical or developmental disabilities, mental health challenges, or other supposrt needs. Trained professionals work closely with clients to create personalized plans that meet their specific needs and goals."
+    }
+]
