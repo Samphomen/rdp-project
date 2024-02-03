764de4b85e5c82aae34038cdad55a99446d889ac
Faruk Alade <farukalade@Faruks-MBP.lan>
2024-02-03 12:07:28 -0500

    Nav bar and footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 City Care. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Logo from "../Images/logo.jpg";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="nav">
+      <div className="logo-container">
+        <img src={Logo} className="logo" alt="Logo" />
+      </div>
+      <div className="nav-items">
+        {["Home", "About", "Blog", "Pricing", "FAQ"].map((item, index) => (
+          <p key={index} className="nav-item">
+            {item}
+          </p>
+        ))}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
